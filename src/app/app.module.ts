5dcb29dd62717e0aedb40aaae2c77ca2e2efceac
Jammerson Lopes <18218774+lopes256@users.noreply.github.com>
2026-09-07 22:16:11 -0300

    #0709 Correcao AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { TemplateModule } from './template/template.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     SecurityModule
